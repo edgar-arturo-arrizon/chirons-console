3f7a241fcf523be7c79617eee36ac33cb462de1d
Eddie <arrizonedgar91@gmail.com>
2022-02-11 10:54:08 -1000

    routes to login and register complete

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,14 +2,14 @@ import React from "react";
 import Register from './Register.js';
 
 
-const Landing = () => {
+const Landing = ({ setAuth }) => {
   return (
     <>
       <div className="lg:grid lg:grid-cols-2">
         <div className="bg-yellow-600 flex justify-center items-start">
           <h1 className="text-6xl font-serif font-extrabold relative  top-40">Chiron's Log</h1>
         </div>
-          <Register />
+          <Register setAuth={setAuth}/>
       </div>
     </>
   );
